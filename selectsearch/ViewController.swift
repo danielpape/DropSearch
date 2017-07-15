672b06a4fe6d4304a852148abf0d58223ab0997b
Daniel Pape <danielwpape@gmail.com>
2017-07-15 14:25:01 +0100

    Initial Commit

---
+++ selectsearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  selectsearch
+//
+//  Created by Daniel Pape on 15/07/2017.
+//  Copyright © 2017 Daniel Pape. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
